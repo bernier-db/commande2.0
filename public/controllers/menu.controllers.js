9c3ef225ad1be66d89a4e034e03a2103f9292e4d
Dave Bernier <bernier.db@gmail.com>
2017-03-29 16:26:38 -0400

    Intégration des pages designées
    Me nu s'Affiche dans page

--- public/controllers/menu.controllers.js
+++ public/controllers/menu.controllers.js
@@ -3,4 +3,25 @@ angular.module("app")
 .controller('MenuCtrl', ['$scope','$http','$cookies', function($scope, $http, $cookies){
     
 	$scope.tableId = $cookies.get('tableId');
+    $http.get('/menu').then(function(data){
+		$scope.meals = data.data.meals;
+        $scope.categories = data.data.categories;
+       
+	});
+    
+     $scope.openMenu = function (evt, menuName) {
+        
+        var i, x, tablinks;
+        x = document.getElementsByClassName("menu");
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";
+        }
+        tablinks = document.getElementsByClassName("tablink");
+        for (i = 0; i < x.length; i++) {
+            tablinks[i].className = tablinks[i].className.replace(" w3-red", "");
+        }
+        document.getElementById(menuName).style.display = "block";
+        //evt.currentTarget.className += " w3-red";
+    }
+    
 }]);
--- routes/menu.js
+++ routes/menu.js
@@ -1,50 +1,60 @@
 var express = require('express');
 var router = express.Router();
 var Meal = require('../models/meal');
+var Category = require('../models/categories');
+router.get('/', function (req, res, next) {
 
-router.get('/', function(req, res, next) {
-    
     //Fetch tous les repas
-    Meal.getMeals(function(err, meals){
-        if(err) {
-            console.log(err);
+    Meal.getMeals(function (err1, meals) {
+        if (err1) {
+            console.log(err1);
         } else {
-            //res.json(meals);
-            res.render('./menu');
+            //fetch categories
+            Category.getCategories(function (err2, c) {
+                if (err2) {
+                    console.log('ERREUURRRRRRRRRRRRRRR', err2);
+                } else
+                    
+                    res.json({
+                        categories: c,
+                        meals: meals
+                    });
+
+            });
         }
     });
-    
-  
+
+
 });
 
 //route pour description:
-router.get('/:id', function(req, res, next) {
-    
-    
-    Meal.getMealById(req.params.id, function(err, meal){
-        if(err) {
+router.get('/:id', function (req, res, next) {
+
+
+    Meal.getMealById(req.params.id, function (err, meal) {
+        if (err) {
             console.log(err);
         } else {
             res.json(meal);
         }
     });
     //res.render('menu.html');
-  
+
 });
 
 
 //route pour categorie
-router.get('/category/:category', function(req, res, next) {
-      
-    Meal.getMealsByCategory(req.params.category, function(err, meal){
-        if(err) {
+router.get('/category/:category', function (req, res, next) {
+
+    Meal.getMealsByCategory(req.params.category, function (err, meal) {
+        if (err) {
             console.log(err);
         } else {
             res.json(meal);
         }
     });
     //res.render('menu.html');
-  
+
 });
 
 module.exports = router;
